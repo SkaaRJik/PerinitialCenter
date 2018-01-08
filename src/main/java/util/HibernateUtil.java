acdd5b0398773886d6015d6e155cc97bc25e1c00
Filippov Victor <filippowvitia@yandex.com>
2018-01-08 11:50:31 +0800

    JavaDoc added

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -10,13 +10,23 @@ import stages.ErrorStage;
  * Рус:
  * <p>
  * Класс для работы с {@link SessionFactory}.
- * Инициализурется единожды
+ * Вызывается для работы с базой данных.
  * <p>
  * Eng:
  * <p>
  * Class for working with {@link SessionFactory}
+ * Called to working with data base.
  */
 public class HibernateUtil{
+    /**
+     * Рус:
+     * <p>
+     * Сессия для работы с базой данных.
+     * <p>
+     * Eng:
+     * <p>
+     * Session works with database
+     */
     private static final SessionFactory sessionFactory;
     static{
         try {
@@ -34,9 +44,28 @@ public class HibernateUtil{
         }
     }
 
+    /**
+     * Рус:
+     * <p>
+     * Геттер {@link #sessionFactory}
+     * <p>
+     * Eng:
+     * Getter of {@link #sessionFactory}
+     * @return
+     */
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+
+    /**
+     * Рус:
+     * <p>
+     * Закрывает сессию, если она есть.
+     * <p>
+     * Eng:
+     * <p>
+     * Close existing session.
+     */
     public static void closeSession() {
         if(sessionFactory!=null)
             HibernateUtil.getSessionFactory().close();
